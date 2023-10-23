b15bb9ee62468587b1a0b6161d8406fd46af433f
“Abhishek <abhinaikds@gmail.com>
2023-10-23 11:50:44 +0530

    third commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,jsonify
+import json
 
 app=Flask(__name__)
 
@@ -6,7 +7,22 @@ app=Flask(__name__)
 def welcome():
     return "welcome to Flask"
 
+@app.route('/cal',methods=['Get'])
+def math_operator():
+    operation=request.json["operation"]
+    number1=request.json["number1"]
+    number2=request.json["number2"]
+
+    if operation =='add':
+        result =int(number1)+int(number2)
+    elif operation == 'subtract':
+        result=int(number1)-int(number2)
+    elif operation == 'multiply':
+        result=int(number1)*int(number2)
+    else:
+        result=int(number1)/int(number2)
+    return jsonify(result)
 
 
-if __name__==__main__:
+if __name__=='__main__':
     app.run()
